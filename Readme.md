70b37098e4895feea8590a8534a3b96bbedfba63
akt <threadend@gmail.com>
2018-03-11 02:12:53 +0300

    add readme

---
+++ Readme.md
@@ -0,0 +1,9 @@
+#### How to build
+ * Execute: *mvn clean package*
+
+#### How to start program
+ 1. Build program
+ 2. execute *java -jar target/money-transfer-api-jar-with-dependencies* to run program with default configuration
+ 3. execute *java -jar target/money-transfer-api-jar-with-dependencies param1 param2 param3* where param1 is httpServer api, param2 httpServer port, param3 port of redis.
+ 
+  
--- src/main/java/akt/app/RunApp.java
+++ src/main/java/akt/app/RunApp.java
@@ -14,36 +14,34 @@ import redis.embedded.RedisServer;
 public class RunApp {
 
 	public static void main(String[] args) throws Exception {
+		String httpServerAddress = ServerAddress.DEFAULT_HTTP_SERVER_ADDRESS;
+		String httpServerPort = ServerAddress.DEFAULT_HTTP_SERVER_PORT + "";
+		String redisPortArg = ServerAddress.DEFAULT_REDIS_PORT + "";
 		if (args.length == 3) {
-			String httpServerAddress = args[0];
-			String httpServerPort = args[1];
-			String redisPortArg = args[2];
+			httpServerAddress = args[0];
+			httpServerPort = args[1];
+			redisPortArg = args[2];
+		}
+		else {
+			System.out.println("Server will be run with default configuration");
+		}
+		try {
+			int redisPort = Integer.parseInt(redisPortArg);
+			ServletMapPathFactory servletMapPathFactory = new ServletMapPathFactory(
+				new UserServiceImpl(new UserDaoImpl(ServerAddress.DEFAULT_REDIS_HOST,
+					redisPort))
+			);
+			HttpServerFactory httpServerFactory = new HttpServerFactory(servletMapPathFactory.create());
 
-			try {
-				int redisPort = Integer.parseInt(redisPortArg);
-				ServletMapPathFactory servletMapPathFactory = new ServletMapPathFactory(
-					new UserServiceImpl(new UserDaoImpl(ServerAddress.DEFAULT_REDIS_HOST,
-						redisPort))
-				);
-				HttpServerFactory httpServerFactory = new HttpServerFactory(servletMapPathFactory.create());
-
-				HttpServer httpServer = httpServerFactory
-					.buildHttpServer(httpServerAddress, Integer.parseInt(httpServerPort), 100);
-				httpServer.start();
-
-				RedisServer redisServer = new RedisServer(redisPort);
-				redisServer.start();
-
-			} catch (NumberFormatException exception) {
-				System.out.println("Port should be integer");
-			}
-		} else {
-			System.out.println("Application will run with default configuration");
-			HttpServer httpServer = HttpServerFactory.DEFAULT;
+			HttpServer httpServer = httpServerFactory
+				.buildHttpServer(httpServerAddress, Integer.parseInt(httpServerPort), 100);
 			httpServer.start();
 
-			RedisServer redisServer = new RedisServer();
+			RedisServer redisServer = new RedisServer(redisPort);
 			redisServer.start();
+
+		} catch (NumberFormatException exception) {
+			System.out.println("Port should be integer");
 		}
 	}
 }
